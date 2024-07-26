ff48764ad358e060016acd072db3873dbf9aa441
Paul Hovley <rphovley@gmail.com>
2024-07-26 11:38:51 -0600

    deliver app assets from nestjs server

--- src/server/src/app.module.ts
+++ src/server/src/app.module.ts
@@ -4,6 +4,8 @@ import { APP_FILTER, RouterModule } from '@nestjs/core'
 import { DbModule } from './db/knex'
 import { AllExceptionsFilter } from '../utils/allExceptions.filter'
 import { RequestLoggerMiddleware } from './middleware/requestlogger.middleware'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 
 @Module({
   imports: [
@@ -15,6 +17,9 @@ import { RequestLoggerMiddleware } from './middleware/requestlogger.middleware'
         module: V1Module,
       },
     ]),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', '..', 'dist/app'),
+    }),
   ],
   providers: [
     {
